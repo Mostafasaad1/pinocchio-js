2899857241ec589b6ebf1dab3a024815f3073201
Mostafasaad1 <mostafa.saad@ejust.edu.eg>
2026-02-20 10:35:11 +0200

    chore: add pinocchio shim headers for CI build

---
+++ src/pinocchio/deprecated.hpp
@@ -0,0 +1,9 @@
+// Shim for Pinocchio generated header
+
+#ifndef PINOCCHIO_DEPRECATED_HPP
+#define PINOCCHIO_DEPRECATED_HPP
+
+#define PINOCCHIO_DEPRECATED __attribute__((deprecated))
+#define PINOCCHIO_DEPRECATED_MESSAGE(msg) __attribute__((deprecated(msg)))
+
+#endif // PINOCCHIO_DEPRECATED_HPP
